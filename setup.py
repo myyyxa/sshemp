a7ef8ba766c42693e94a06855b319b7ae184964e
myyyxa <myyyxa@gmail.com>
2020-09-25 10:32:16 +0300

    requirement,setup,inits,ota_updater

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sshemp',
+    version='0.2020.09.25.1028',
+    packages=['main'],
+    url='https://github.com/myyyxa/sshemp',
+    license='free',
+    author='myyyx',
+    author_email='myyyxa@gmail.com',
+    description='simple smart house esp32 micropython'
+)
